ef2e8d4eea480fb2e3b2703eb3693150c3132558
BryanG <47282239+BryanGamingDV@users.noreply.github.com>
2023-03-03 19:25:15 -0500

    feat(services): add services to bind in set and start in main plugin.

--- src/main/java/me/bryang/effectranks/EffectRanks.java
+++ src/main/java/me/bryang/effectranks/EffectRanks.java
@@ -1,23 +1,31 @@
 package me.bryang.effectranks;
 
 import me.bryang.effectranks.modules.MainModule;
+import me.bryang.effectranks.services.Service;
 import org.bukkit.plugin.java.JavaPlugin;
 import team.unnamed.inject.Injector;
 
+import javax.inject.Inject;
+import java.util.Set;
+
 public class EffectRanks extends JavaPlugin {
 
+    @Inject
+    private Set<Service> services;
 
 
     @Override
     public void onEnable() {
 
-        Injector injector = Injector.create(new MainModule(this));
+        Injector injector = Injector
+                .create(new MainModule(this));
         injector.injectMembers(this);
 
+        services.forEach(Service::init);
     }
 
     @Override
     public void onDisable() {
-        // Plugin shutdown logic
+
     }
 }
--- src/main/java/me/bryang/effectranks/modules/ServicesModule.java
+++ src/main/java/me/bryang/effectranks/modules/ServicesModule.java
@@ -1,4 +1,24 @@
 package me.bryang.effectranks.modules;
 
-public class ServicesModule {
+import me.bryang.effectranks.EffectRanks;
+import me.bryang.effectranks.manager.GroupManager;
+import me.bryang.effectranks.services.CommandService;
+import me.bryang.effectranks.services.ListenerService;
+import me.bryang.effectranks.services.Service;
+import me.fixeddev.commandflow.annotated.CommandClass;
+import team.unnamed.inject.AbstractModule;
+
+public class ServicesModule extends AbstractModule {
+
+    @Override
+    protected void configure() {
+
+        multibind(Service.class)
+                .asSet()
+                .to(CommandService.class)
+                .to(ListenerService.class)
+                .to(ManagerService.class);
+
+
+    }
 }
